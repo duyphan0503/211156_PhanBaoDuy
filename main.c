4a0d175e1fd1e51c6e66e8f5c2ca86f557f55158
Phan Bao Duy <duyphanz0v0z@gmail.com>
2024-10-21 15:25:46 +0700

    added shellc1, shellc2, main

---
+++ main.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+extern void shellc1();
+extern void shellc2();
+
+int main() {
+	shellc1();
+	printf("\n");
+	shellc2();
+	return 0;
+}
---
+++ shellc1.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+void shellc1() {
+	printf("This is shellc1");
+}
+
